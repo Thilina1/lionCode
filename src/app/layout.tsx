1ea8b7a5afd562bea80712aa79a8238430180821
Thilina Weerasinghe <thilinaweeras@gmail.com>
2026-01-15 07:15:03 +0000

    in every pages please add space from top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         >
           <div className="relative flex min-h-screen flex-col">
             <Header />
-            <main className="flex-1">{children}</main>
+            <main className="flex-1 pt-[120px]">{children}</main>
             <Footer />
           </div>
           <ChatWidget />
